2232dd5e9dcd1971690f3795e10905770a1b6da8
Kasidhad <kkasidhad@gmail.com>
2024-11-09 22:08:32 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import todoRouter from '../server/routes/todoRouter.js'
+import userRouter from '../server/routes/userRouter.js'
+
+const port = process.env.PORT
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/', todoRouter)
+app.use('/user', userRouter)
+
+app.listen(port)
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({error: err.message})
+})
+
---
+++ server/index.test.js
@@ -0,0 +1,117 @@
+import { initializeTestDb, insertTestUser } from "./helper/test.js"
+import { getToken } from "../server/helper/test.js"
+
+const base_url = "http://localhost:3001"
+
+
+import { expect } from "chai"
+
+describe('GET task', () =>{
+    before(() => {
+        initializeTestDb()
+    })
+
+    it ('should get all tasks', async() => {
+        const response = await fetch('http://localhost:3001')
+        const data = await response.json()
+
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an('array').that.is.not.empty
+        expect(data[0]).to.include.all.keys('id','description')
+    })
+})
+
+describe('POST task',() => {
+    const email = 'post@foo.com'
+    const password = 'post123'
+    insertTestUser(email,password)
+    const token = getToken(email)
+    it('should post a task', async () => {
+        const response = await fetch(base_url + '/create', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token
+            },
+            body: JSON.stringify({ 'description': 'Task from unit test'})
+        });
+        const data = await response.json()
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id')
+    })
+
+    it('should not post a task without description', async () => {
+        const response = await fetch(base_url + '/create', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'description': null })
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(500)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('error')
+    })  
+})
+
+
+describe('DELETE task', () => {
+    it('should delete a task', async () => {
+        const response = await fetch(base_url + '/delete/1', {
+            method: 'delete'
+        });
+        const data = await response.json();
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id')
+    })
+
+    it('should not delete a task with SQL injection', async () => {
+        const response = await fetch(base_url + '/delete/id=0 or id > 0', {
+            method: 'delete'
+        });
+        const data = await response.json()
+        expect(response.status).to.equal(500)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('error')
+    })
+})
+
+describe('POST register', () => {
+    const email = 'register@foo.com'
+    const password = 'reigster123'
+    it ('should register with valid email and password', async() =>{
+        const response = await fetch(base_url + '/user/register',{
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'email':email, 'password': password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(500,data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('error'/*'id','email'*/)
+    })
+})
+
+describe('POST login', () => {
+    const email = 'login@foo.com'
+    const password = 'login123'
+    insertTestUser(email,password)
+    it ('should login with valid credentials', async() => {
+        const response = await fetch(base_url + '/user/login', {
+            method: 'post', 
+            headers: {
+                'Content-Type': 'applications/json'
+            },
+            body: JSON.stringify({'email':email, 'password': password})
+        })
+        const data = await response.json()
+        expect(response.status).equal(200,data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id','email','token')
+    })
+})
